22a7e1607e460d6f4007b66ac427f115b02af543
KBAKEP <alexey.volkov.blr@gmail.com>
2016-02-01 17:11:11 +0300

    1. Implement asynchronous message bus. Do not use queue implementations from  java.util.concurrent.
    2. Implement producer, which will generate and post randomly messages to
    the queue
    
    3. Implement consumer, which will consume messages on specific topic and
    log to the console message payload
    
    Optional: 4.  Application should create several consumers and producers
    that run in parallel.

--- com.epam.mentoring.lessfour/src/com/epam/mentoring/module8/task6/MTRunner.java
+++ com.epam.mentoring.lessfour/src/com/epam/mentoring/module8/task6/MTRunner.java
@@ -2,20 +2,53 @@ package com.epam.mentoring.module8.task6;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
 
 public class MTRunner {
 
-	public class Message {
+	private static int ITERATIONS = 10;
+	
+	public static void main(String... args) {
+		Queue<Message> queue = new LinkedList<>();
+		AsyncMessageBus asyncMessageBus = new AsyncMessageBus(queue);
+		
+		for (int i = 0; i < 10; i++) {
+			Thread prod = new Thread(new Producer(asyncMessageBus));
+			Thread cons = new Thread(new Consumer(asyncMessageBus));
+			
+			prod.start();
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			cons.start();
+		}
+		
+		
+	
+	}
+
+	static public class Message {
 		String payload;
 
 		public Message(String payload) {
 			this.payload = payload;
 		}
+
+		@Override
+		public String toString() {
+			return "Message [payload= " + payload + "]";
+		}
 	}
 
-	public class AsyncMessageBus {
+	static public class AsyncMessageBus {
 
-		Queue<Message> queue = new LinkedList<>();
+		Queue<Message> queue;
+
+		public AsyncMessageBus(Queue<Message> queue2) {
+			queue = queue2;
+		}
 
 		public void add(Message integer) {
 			synchronized (queue) {
@@ -23,13 +56,61 @@ public class MTRunner {
 			}
 		}
 
-		public void remove() {
+		public Message remove() {
 			synchronized (queue) {
-				queue.remove();
+				return queue.remove();
 			}
 		}
 
 	}
 
-	
+	static class Producer implements Runnable {
+
+		AsyncMessageBus messageBus;
+
+		public Producer(AsyncMessageBus bus) {
+			messageBus = bus;
+		}
+
+		@Override
+		public void run() {
+			for (int i = 0; i < ITERATIONS; i++) {
+				String payload = "i=" + (new Random().nextInt(990) + 10);
+				Message message = new Message(payload);
+				messageBus.add(message);
+
+				System.out.println(Thread.currentThread().getName() + " Producer " + message);
+				
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	static class Consumer implements Runnable {
+
+		AsyncMessageBus messageBus;
+
+		public Consumer(AsyncMessageBus bus) {
+			messageBus = bus;
+		}
+
+		@Override
+		public void run() {
+			for (int i = 0; i < ITERATIONS; i++) {
+				System.out.println(Thread.currentThread().getName() + " Consumer " + messageBus.remove());
+			
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
 }
