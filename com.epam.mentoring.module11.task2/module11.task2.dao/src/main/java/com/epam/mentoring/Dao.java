172bd882000f3c5734b053829a9d80a1497a5029
Aliaksei_Volkau <Aliaksei_Volkau@EPBYMINW4540.minsk.epam.com>
2016-02-20 22:15:56 +0300

    Module: # Java CDP BY 11 Infrastructure: Build: Maven, Gradle Gradle and Maven Task
    --GRADLE project 
    initial commit

---
+++ com.epam.mentoring.module11.task2/module11.task2.dao/src/main/java/com/epam/mentoring/Dao.java
@@ -0,0 +1,11 @@
+package com.epam.mentoring;
+
+public class Dao {
+
+    public static final String DESCR = "DAO";
+    
+    public static void main(String[] args) {
+
+        System.out.println("Hello DAO!");
+    }
+}
---
+++ module11.task1/module11.task1.web/src/main/java/com/epam/mentoring/App.java
@@ -0,0 +1,14 @@
+package com.epam.mentoring;
+
+import com.epam.mentoring.service.Service;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        Service.service();
+    }
+}
